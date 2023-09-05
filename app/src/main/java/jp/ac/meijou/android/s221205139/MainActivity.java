82940c73c772dfd6325ac2526faeaa7b0e147504
Takumi Hirabayashi <221205139@ccmailg.meijo-u.ac.jp>
2023-09-05 15:42:46 +0900

    prefDataStoreまでの実装

--- app/src/main/java/jp/ac/meijou/android/s221205139/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s221205139/MainActivity.java
@@ -12,17 +12,28 @@ import jp.ac.meijou.android.s221205139.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        prefDataStore = PrefDataStore.getInstance(this);
+
+        prefDataStore.getString("name")
+                .ifPresent(name -> binding.textView.setText(name));
 
         binding.button.setOnClickListener(view -> {
             var text = binding.editTextText.getText().toString();
             binding.textView.setText(text);
         });
+
+        binding.saveButton.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
+        });
+
 
         binding.editTextText.addTextChangedListener(new TextWatcher() {
             @Override
