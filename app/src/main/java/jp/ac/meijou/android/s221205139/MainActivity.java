ebf9b87da67f6defde9eb184d6bf29bf2ebc85ad
Takumi Hirabayashi <221205139@ccmailg.meijo-u.ac.jp>
2023-09-05 13:39:47 +0900

    テキストボックス，ボタンを追加

--- app/src/main/java/jp/ac/meijou/android/s221205139/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s221205139/MainActivity.java
@@ -3,12 +3,43 @@ package jp.ac.meijou.android.s221205139;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import jp.ac.meijou.android.s221205139.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            binding.textView.setText(text);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.textView.setText(s.toString());
+            }
+        });
+
     }
 }
